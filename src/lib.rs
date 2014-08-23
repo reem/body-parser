9ef1561b0f3a677d2e34b3c0080a5b3a7065fbe0
zzmp <zmp@umich.edu>
2014-08-23 09:56:36 -0700

    (docs) Name and href.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/7853871?s=128", html_favicon_url = "https://avatars0.githubusercontent.com/u/7853871?s=256", html_root_url = "http://ironframework.io/core/bodyparser")]
+#![crate_name = "body-parser"]
 //! Body Parser middleware for Iron
 //!
 //! This middleware parses incoming JSON data from client requests. On an empy
